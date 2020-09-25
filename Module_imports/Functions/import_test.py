4224a0d2f24475badf10e280a37b030ea419a362
William Costa <wd.costa@campus.fct.unl.pt>
2020-09-25 10:13:26 +0100

    24th september

--- Module_imports/Functions/import_test.py
+++ Module_imports/Functions/import_test.py
@@ -2,4 +2,11 @@ import blackjack
 
 print(__name__)
 
-# blackjack.create_game()
+blackjack.create_game()
+print(blackjack.cards)
+
+personal_details = ("Tim", 24, "Australia")
+
+name, _, country = personal_details
+print(name, country)
+print(_)
